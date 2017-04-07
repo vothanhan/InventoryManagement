51262ddd23f5f0f4efd3ad439ae97c3aecd41527
Vo Thanh An <vothanhan265@gmail.com>
2017-04-07 14:31:45 +0700

    To onine db

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	//url:'mongodb://vothanhan:CS422InventoryManagement@ds129260.mlab.com:29260/myangulartesting'
-	url:'mongodb://127.0.0.1/Testing'
+	url:'mongodb://vothanhan:CS422InventoryManagement@ds129260.mlab.com:29260/myangulartesting'
+	//url:'mongodb://127.0.0.1/Testing'
 }
